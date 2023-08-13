303a3e24fde5691da506aaf7b7e77fa65d1574cf
Corentin <corentin.lechene.pro@gmail.com>
2023-08-13 17:44:32 +0200

    feat(movie-detail): rework MovieDetails.vue component and add note

--- src/views/MovieDetails/MovieDetails.vue
+++ src/views/MovieDetails/MovieDetails.vue
@@ -1,70 +1,27 @@
 <script setup lang="ts">
-import {
-  IonContent,
-  IonPage,
-  IonToolbar,
-  IonBackButton,
-  IonButtons,
-  IonTitle,
-  IonFooter,
-  IonModal,
-  IonTextarea,
-  IonList,
-  IonItem,
-  IonListHeader,
-  IonInput,
-  IonIcon,
-  IonText,
-} from "@ionic/vue";
+import {IonContent, IonPage, IonFooter, IonText} from "@ionic/vue";
+import {onMounted, ref} from "vue";
 import {useRouter} from "vue-router";
-import {computed, onMounted, reactive, ref} from "vue";
-import {TheMovieDbService} from "@/services/the-movie-db.service";
-import {Movie, TheMovieDb, ViewedMovie} from "@/models";
-import AppHeader from "@/components/headers/BaseHeader.vue";
-import AddToList from "@/components/cards/AddToList.vue";
-import dayjs from "dayjs";
-import AppButton from "@/components/buttons/AppButton.vue";
-import {arrowBack} from "ionicons/icons";
 import {useUserStore} from "@/stores/user";
-import {MovieService} from "@/services/movie.service";
 
-interface FormViewedMovie {
-  date: string;
-  extra: 0 | number;
-  note?: string;
-}
+import {Movie, ViewedMovie} from "@/models";
+import {MovieService} from "@/services/movie.service";
+import dayjs from "@/configs/dayjs.config";
+
+import AddToList from "@/components/cards/AddToList.vue";
+import AppButton from "@/components/buttons/AppButton.vue";
+import Header from "@/components/headers/Header.vue";
+
 
 const router = useRouter();
 const id = router.currentRoute.value.params.id as string;
 
 const userStore = useUserStore();
 
-let movie = ref<Movie>();
+let movie = ref<Movie | undefined>();
+let viewedMovie = ref<ViewedMovie | undefined>(undefined);
 const openModalViewMovie = ref(false);
 
-const formViewedMovie = reactive<FormViewedMovie>({
-  date: dayjs().toISOString().split('T')[0],
-  extra: 0,
-  note: "",
-});
-
-const isMovieViewed = computed(() => {
-  if(!userStore.user) {
-    return false;
-  }
-  return userStore.user.viewedMovies.some(m => String(m.movie.id) === id);
-});
-
-const viewedAt = computed(() => {
-  if(!userStore.user) {
-    return null;
-  }
-  const viewedMovie = userStore.user.viewedMovies.find(m => String(m.movie.id) === id);
-  if(!viewedMovie) {
-    return null;
-  }
-  return dayjs(viewedMovie.viewedAt).format('DD/MM/YYYY');
-});
 
 onMounted(async () => {
   userStore.loadUser().catch();
@@ -75,42 +32,18 @@ onMounted(async () => {
     return;
   }
   movie.value = result;
+
+  if(userStore.user) {
+    viewedMovie.value = userStore.user.viewedMovies.find(m => String(m.movie.id) === id);
+  }
 });
 
-
-function saveMovie() {
-
-  userStore.loadUser()
-      .then(() => {
-        if(!userStore.user) {
-          return;
-        }
-        const movieViewed: ViewedMovie = {
-          viewedAt: dayjs(formViewedMovie.date).toDate(),
-          movie: movie.value!,
-          extra: formViewedMovie.extra,
-          note: formViewedMovie.note || '',
-          subscription: userStore.user.subscriptions[userStore.user.subscriptions.length - 1],
-        };
-        userStore.addMovieToViewed(movieViewed);
-        userStore.removeFromWatchList(movie.value!);
-      });
-  openModalViewMovie.value = false;
-}
 
 </script>
 
 <template>
   <ion-page>
-
-    <AppHeader class="flex flex-row justify-between pb-3">
-      <ion-toolbar>
-        <ion-buttons slot="start">
-          <ion-back-button></ion-back-button>
-        </ion-buttons>
-        <ion-title color="dark" class="text-2xl">Détail du film</ion-title>
-      </ion-toolbar>
-    </AppHeader>
+    <Header title="Détail du film" back-button default-href="/home" />
 
     <ion-content>
       <div v-if="movie" class="px-4">
@@ -142,75 +75,21 @@ function saveMovie() {
           <ion-text color="dark" class="text-2xl mt-4 mb-2">Synopsis</ion-text>
           <ion-text color="medium">{{ movie.overview }}</ion-text>
         </div>
+
+        <div v-if="viewedMovie && viewedMovie.note" class="flex flex-col">
+          <ion-text color="dark" class="text-2xl mt-4 mb-2">Ma note personnelle</ion-text>
+          <ion-text color="medium">{{ viewedMovie.note }}</ion-text>
+        </div>
       </div>
 
       <!-- modal       -->
-      <ion-modal trigger="open-modal" :is-open="openModalViewMovie" :initial-breakpoint="1" :breakpoints="[0, 1]">
-        <div class="">
-
-          <AppHeader class="flex flex-row justify-between pb-3">
-            <ion-toolbar>
-              <ion-icon :icon="arrowBack" size="small"></ion-icon>
-              <ion-title color="dark" class="text-2xl">Enregistrer</ion-title>
-            </ion-toolbar>
-          </AppHeader>
-
-          <ion-list class="pr-4">
-
-            <ion-list-header>
-              <ion-text color="dark" class="font-medium">Date</ion-text>
-            </ion-list-header>
-            <ion-item>
-              <ion-input
-                  :model-value="formViewedMovie.date"
-                  aria-text="Date"
-                  type="date"
-              />
-            </ion-item>
-
-            <ion-list-header>
-              <ion-text color="dark" class="font-medium">Achats supplémentaires</ion-text>
-            </ion-list-header>
-            <ion-item>
-              <ion-input
-                  :model-value="formViewedMovie.extra"
-                  aria-text="Achat supplémentaire"
-                  type="number"
-                  placeholder="Achat supplémentaire"
-              />
-            </ion-item>
-
-            <ion-list-header>
-              <ion-text color="dark" class="font-medium">Note personnelle</ion-text>
-            </ion-list-header>
-            <ion-item>
-              <ion-textarea
-                  :model-value="formViewedMovie.note"
-                  aria-text="Note personnelle"
-                  placeholder="Note personnelle"
-                  :rows="3"
-              />
-            </ion-item>
-          </ion-list>
-
-          <div class="px-4 py-4">
-            <app-button
-                color="dark"
-                text="Enregistrer"
-                @onTap="saveMovie()"
-            />
-
-          </div>
-
-        </div>
-      </ion-modal>
     </ion-content>
 
     <ion-footer class="py-4 px-4 flex">
-      <ion-text v-if="isMovieViewed" color="medium" class="italic text-lg m-auto">
-        Visionné le {{viewedAt}}
+      <ion-text v-if="viewedMovie" color="medium" class="italic text-lg m-auto">
+        Visionné le {{ dayjs(viewedMovie.viewedAt).format('DD/MM/YYYY') }}
       </ion-text>
-      <app-button v-else id="open-modal" color="dark" text="J'ai vu ce film" @onTap="openModalViewMovie = true" />
+      <app-button v-else color="dark" text="J'ai vu ce film" @onTap="openModalViewMovie = true" />
     </ion-footer>
 
   </ion-page>
