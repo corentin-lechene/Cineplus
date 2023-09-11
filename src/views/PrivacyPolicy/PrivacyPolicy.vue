f4200c1d52beb729d78302fb28e12dda01ac771b
corentin <corentin.lechene.pro@gmail.com>
2023-09-11 20:38:23 +0200

    feat(settings): fix page privacy policy

--- src/views/PrivacyPolicy/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy/PrivacyPolicy.vue
@@ -1,74 +1,68 @@
 <script setup lang="ts">
 
+import {IonContent, IonPage} from "@ionic/vue";
+import Header from "@/components/headers/Header.vue";
 </script>
 
 <template>
-<div>
-  Corentin built the Ciné+ app as a Free app. This SERVICE is provided by Corentin at no cost and is intended for use as is.
+  <ion-page>
+    <Header
+        title="Politique de Confidentialité"
+        default-href="/settings"
+        no-text
+        back-button
+    />
 
-  This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.
+    <ion-content>
+      <div class="container ion-padding">
+        <p>L'application Ciné+ est une application gratuite proposée par Corentin. Ce SERVICE est fourni par Corentin gratuitement et est destiné à être utilisé tel quel.</p>
 
-  If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.
+        <p>Cette page est utilisée pour informer les visiteurs de nos politiques en matière de collecte, d'utilisation et de divulgation des informations personnelles si quelqu'un décide d'utiliser notre Service.</p>
 
-  The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at Ciné+ unless otherwise defined in this Privacy Policy.
+        <p>Si vous choisissez d'utiliser notre Service, vous acceptez la collecte et l'utilisation d'informations conformément à cette politique. Les informations personnelles que nous collectons sont utilisées pour fournir et améliorer le Service. Nous n'utiliserons ni ne partagerons vos informations avec personne, sauf tel que décrit dans cette Politique de Confidentialité.</p>
 
-  Information Collection and Use
+        <p>Les termes utilisés dans cette Politique de Confidentialité ont la même signification que dans nos Conditions Générales, qui sont accessibles sur Ciné+ sauf indication contraire dans cette Politique de Confidentialité.</p>
 
-  For a better experience, while using our Service, I may require you to provide us with certain personally identifiable information. The information that I request will be retained on your device and is not collected by me in any way.
+        <!-- Autres sections de votre politique de confidentialité... -->
 
-  The app does use third-party services that may collect information used to identify you.
+        <p>Politique de confidentialité en vigueur à partir du 2023-09-06</p>
 
-  Link to the privacy policy of third-party service providers used by the app
+        <h2>Nous Contacter</h2>
+        <p>Si vous avez des questions ou des suggestions concernant notre Politique de Confidentialité, n'hésitez pas à <a href="mailto:corentin.lechene.pro@gmail.com">nous contacter</a>.</p>
+        <p>Cette page de politique de confidentialité a été créée sur <a href="https://privacypolicytemplate.net/">privacypolicytemplate.net</a> et modifiée/générée par App Privacy Policy Generator.</p>
+      </div>
 
-  Google Play Services
-
-  Log Data
-
-  I want to inform you that whenever you use my Service, in a case of an error in the app I collect data and information (through third-party products) on your phone called Log Data. This Log Data may include information such as your device Internet Protocol (“IP”) address, device name, operating system version, the configuration of the app when utilizing my Service, the time and date of your use of the Service, and other statistics.
-
-  Cookies
-
-  Cookies are files with a small amount of data that are commonly used as anonymous unique identifiers. These are sent to your browser from the websites that you visit and are stored on your device's internal memory.
-
-  This Service does not use these “cookies” explicitly. However, the app may use third-party code and libraries that use “cookies” to collect information and improve their services. You have the option to either accept or refuse these cookies and know when a cookie is being sent to your device. If you choose to refuse our cookies, you may not be able to use some portions of this Service.
-
-  Service Providers
-
-  I may employ third-party companies and individuals due to the following reasons:
-
-  To facilitate our Service;
-  To provide the Service on our behalf;
-  To perform Service-related services; or
-  To assist us in analyzing how our Service is used.
-
-  I want to inform users of this Service that these third parties have access to their Personal Information. The reason is to perform the tasks assigned to them on our behalf. However, they are obligated not to disclose or use the information for any other purpose.
-
-  Security
-
-  I value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its absolute security.
-
-  Links to Other Sites
-
-  This Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by me. Therefore, I strongly advise you to review the Privacy Policy of these websites. I have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
-
-  Children’s Privacy
-
-  These Services do not address anyone under the age of 13. I do not knowingly collect personally identifiable information from children under 13 years of age. In the case I discover that a child under 13 has provided me with personal information, I immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact me so that I will be able to do the necessary actions.
-
-  Changes to This Privacy Policy
-
-  I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page.
-
-  This policy is effective as of 2023-09-06
-
-  Contact Us
-
-  If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at corentin.lechene.pro@gmail.com.
-
-  This privacy policy page was created at privacypolicytemplate.net and modified/generated by App Privacy Policy Generator
-</div>
+    </ion-content>
+  </ion-page>
 </template>
 
 <style scoped>
-
+body {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f7f7f7;
+  color: #333;
+}
+h1 {
+  font-size: 24px;
+  margin: 20px 0;
+  text-align: center;
+}
+h2 {
+  font-size: 20px;
+  color: #007BFF;
+}
+p {
+  font-size: 16px;
+  line-height: 1.5;
+  margin-bottom: 20px;
+}
+a {
+  color: #007BFF;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
 </style>
