9cf5df39e04e53573a921873da0ba13c0b70c83d
corentin <corentin.lechene.pro@gmail.com>
2023-09-23 10:39:43 +0200

    feat(headers): center text

--- src/components/headers/Header.vue
+++ src/components/headers/Header.vue
@@ -15,7 +15,7 @@ interface HeaderProps {
 const props = defineProps<HeaderProps>();
 
 const builtText = computed(() => {
-  if(!props.noText) return 'Retour';
+  if (!props.noText) return 'Retour';
   return props.text || '';
 })
 
@@ -25,9 +25,15 @@ const builtText = computed(() => {
   <BaseHeader>
     <ion-toolbar>
       <ion-buttons v-if="backButton" slot="start">
-        <ion-back-button :text="builtText" :defaultHref="defaultHref" />
+        <ion-back-button :text="builtText" :defaultHref="defaultHref"/>
       </ion-buttons>
-      <ion-title color="dark" class="text-2xl font-medium px-2">{{ title }}</ion-title>
+      <ion-title
+          color="dark"
+          class="text-2xl font-medium px-2"
+          :class="{'text-center': !backButton}"
+      >
+        {{ title }}
+      </ion-title>
     </ion-toolbar>
   </BaseHeader>
 </template>
