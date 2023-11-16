acab0db80d985a8b200172f59cd8d09721e46af5
Corentin <corentin.lechene.pro@gmail.com>
2023-11-16 20:31:54 +0100

    feat(pages): add home, dashboard, search pages

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+
+import {getPlatforms, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} from "@ionic/vue";
+import {grid, home, search} from "ionicons/icons";
+import {computed} from "vue";
+
+// todo create global utils
+const isIos = computed(() => {
+  const platforms = getPlatforms();
+  const ios = ["ios", "ipad", "iphone"];
+  return platforms.find(p => ios.includes(p));
+});
+
+</script>
+
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom" :class="{'rounded-b-3xl': isIos}" class="m-1.5 py-2 rounded-lg">
+        <ion-tab-button href="/home" tab="home">
+          <ion-icon :icon="home"/>
+          <ion-label>Accueil</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button href="/dashboard" tab="library">
+          <ion-icon :icon="grid"/>
+          <ion-label>Dashboard</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button href="/movies/search" tab="search">
+          <ion-icon :icon="search"/>
+          <ion-label>Rechercher</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+  </ion-page>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,60 +1,115 @@
 import {createRouter, createWebHistory} from '@ionic/vue-router';
 import {RouteRecordRaw} from 'vue-router';
-import Home from "@/views/Home/Home.vue";
-import {isConfigured} from "@/router/guards";
+import HomePage from '@/views/HomePage/HomePage.vue'
+import MainLayout from "@/layouts/MainLayout.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/home'
+    redirect: '/home',
+  },
+  {
+    path: '/',
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        redirect: '/home',
+      },
+      {
+        path: 'home',
+        component: () => import('@/views/HomePage/HomePage.vue'),
+      },
+      {
+        path: 'dashboard',
+        component: () => import('@/views/DashboardPage/DashboardPage.vue'),
+      },
+      {
+        path: 'movies/search',
+        component: () => import('@/views/MoviesSearchPage/MoviesSearchPage.vue'),
+      },
+    ],
+  },
+  /*{
+    path: '/onboarding',
+    component: () => import('@/views/Onboarding/Onboarding.vue'),
+  },*/
+  {
+    path: '/',
+    redirect: '/home',
   },
   {
     path: '/home',
-    component: Home,
-  },
-  {
-    path: '/intro',
-    component: () => import('@/views/Intro/Intro.vue')
-  },
-  // {
-  //   path: '/subscriptions',
-  //   component: () => import('@/views/Intro/Subscriptions')
-  // },
-  {
-    path: '/movie-details/:id',
-    component: () => import('@/views/MovieDetails/MovieDetails.vue')
-  },
-  {
-    path: '/search-movie',
-    component: () => import('@/views/SearchMovie/SearchMovie.vue')
-  },
+    component: HomePage,
+  }
   /*{
+    path: '/movies/search',
+    component: () => import('@/views/Movies/SearchMovies.vue'),
+  },
+  {
+    path: '/movies/watch-list',
+    component: () => import('@/views/Movies/WatchList.vue'),
+  },
+  {
+    path: '/movies/watched',
+    component: () => import('@/views/Movies/WatchedMovies.vue'),
+  },
+  {
+    path: '/movies/:movie_id/movie-details',
+    component: () => import('@/views/MovieDetails/MovieDetails.vue'),
+  },
+  {
     path: '/dashboard',
-    component: () => import('@/views/Dashboard/Dashboard.vue')
-  },*/
+    component: () => import('@/views/Dashboard/Dashboard.vue'),
+  },
   {
     path: '/settings',
     component: () => import('@/views/Settings/Settings.vue'),
   },
   {
-    path: '/settings/my-account',
-    component: () => import('@/views/Settings/components/MyAccount.vue')
+    path: 'account',
+    component: () => import('@/views/Settings/Account.vue'),
   },
   {
-    path: '/settings/my-subscriptions',
-    component: () => import('@/views/Settings/components/MySubscriptions.vue')
+    path: 'loyalty-cards',
+    component: () => import('@/views/Settings/LoyaltyCards.vue'),
   },
   {
-    path: '/settings/privacy-policy',
-    component: () => import('@/views/PrivacyPolicy/PrivacyPolicy.vue')
-  }
-]
+    path: 'loyalty-cards/new',
+    component: () => import('@/views/Settings/NewLoyaltyCard.vue'),
+  },
+  {
+    path: 'subscription',
+    component: () => import('@/views/Settings/Subscription.vue'),
+  },
+  {
+    path: 'subscription/new',
+    component: () => import('@/views/Settings/NewSubscription.vue'),
+  },
+  {
+    path: 'advertisement',
+    component: () => import('@/views/Settings/Advertisement.vue'),
+  },
+  {
+    path: 'dark-mode',
+    component: () => import('@/views/Settings/DarkMode.vue'),
+  },
+  {
+    path: 'language',
+    component: () => import('@/views/Settings/Language.vue'),
+  },
+  {
+    path: 'notifications',
+    component: () => import('@/views/Settings/Notifications.vue'),
+  },*/
+];
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 });
 
-router.beforeEach(isConfigured());
+// router.beforeEach(isConfigured());
 
 export default router
