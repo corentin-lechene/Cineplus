83da0af5c01aee41bce2e103d5a83361fca5f989
corentin lechêne <corentin.lechene.pro@gmail.com>
2024-01-13 11:17:16 +0100

    feat(live-updates): test live updates

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'com.corentinlechene.cineplus',
   appName: 'cineplus',
   webDir: 'dist',
+  plugins: {
+    LiveUpdates: {
+      appId: "124d77a8",
+      channel: "Production",
+      autoUpdateMethod: "background",
+      maxVersions: 2
+    }
+  },
   server: {
     androidScheme: 'https'
   }
--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,38 @@
 
 <script setup lang="ts">
 import {IonApp, IonRouterOutlet} from '@ionic/vue';
+import {App} from "@capacitor/app";
+import * as LiveUpdates from '@capacitor/live-updates';
+import {onMounted} from "vue";
+import {Capacitor} from "@capacitor/core";
+
+
+onMounted(() => {
+  try {
+    initializeApp()
+  } catch(e) {
+    console.error(e);
+  }
+})
+
+async function initializeApp() {
+  if(Capacitor.getPlatform() !== 'web') {
+    // Register event to fire each time user resumes the app
+    App.addListener('resume', async () => {
+      if (localStorage.shouldReloadApp) {
+        await LiveUpdates.reload();
+        console.log("[LiveUpdates]: Reload");
+      }
+      else {
+        const result = await LiveUpdates.sync();
+        localStorage.shouldReloadApp = result.activeApplicationPathChanged;
+        console.log(`[LiveUpdates]: Sync: ${JSON.stringify(result)}`);
+      }
+    });
+
+    // First sync on app load
+    const result = await LiveUpdates.sync();
+    localStorage.shouldReloadApp = result.activeApplicationPathChanged;
+  }
+}
 </script>
